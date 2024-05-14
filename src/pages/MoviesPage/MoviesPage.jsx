5bedc6cd49651aea8f9f9a4291d634189a5356db
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-05-14 10:35:02 +0300

    done

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,16 +1,18 @@
 import SearchBar from '../../components/SearchBar/SearchBar'
 import MovieList from '../../components/MovieList/MovieList'
 import Loading from '../../components/Loading/Loading'
-import Error from '../../components/Error/Error'
+import css from './MoviesPage.module.css'
 
 export default function MoviePage({data, onSubmit, error, loading}) {
 
+    console.log(data)
 
     return (
         <div>
             <SearchBar onSubmit={onSubmit}></SearchBar>
             {loading && <Loading></Loading>}
-            {(data?.length > 0 && !error) ? <MovieList data={data}></MovieList> : <Error></Error>}
+            {!error && <MovieList data={data}></MovieList>}
+            {data.length === 0 && <p className={css.noFilm}>No films with such title</p>}
         </div>
     )
 }
